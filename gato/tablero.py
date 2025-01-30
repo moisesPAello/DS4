4d44dfe276fcf4b23f4854cffdcc61bd8d28961e
moisesPAello <a223208792@unison.mx>
2025-01-30 14:23:17 -0700

    Se agrego la estrategia de la ia

---
+++ gato/tablero.py
@@ -0,0 +1,43 @@
+'''
+tablero.py: Dibuja el tablero del juego del gato
+'''
+import random
+
+def dibuja_tablero(simbolos: dict):
+    '''
+    Dibuja tablero del juego de el gato
+    '''
+    print(f'''
+    {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
+    ---------
+    {simbolos['4']} | {simbolos['5']} | {simbolos['6']}
+    ---------
+    {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
+    ''')
+
+def ia(simbolos: dict):
+    """Estrategia de la computadora"""
+    ocupado = True
+    while ocupado is True:
+        x = random.choice(list(simbolos.keys()))
+        if simbolos[x] not in ["X", "O"]:
+            simbolos[x] = "O"
+            ocupado = False
+    
+if __name__ == '__main__':
+        numeros = [str(i) for i in range(1,10)]
+        dsimbolos = {x:x for x in numeros}
+        dibuja_tablero(dsimbolos)
+        ia(dsimbolos)
+        dibuja_tablero(dsimbolos)
+
+
+        """x = random.choice(numeros)
+        numeros.remove(x)
+        dsimbolos[x] = 'X'
+        dibuja_tablero(dsimbolos)
+        o = random.choice(numeros)
+        numeros.remove(o)
+        dsimbolos[o] = 'O'
+        dibuja_tablero(dsimbolos)
+        print(numeros)"""
