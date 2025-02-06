cd1836b0998ffa8ae7efbdbad05a12ec999945c0
moisesPAello <a223208792@unison.mx>
2025-02-06 14:23:04 -0700

    Se implemeto el sistema de score utilizando diccionarios

--- gato/src/app.py
+++ gato/src/app.py
@@ -6,13 +6,28 @@ import tablero
 
 def main():
     """Funcion principal"""
+    X = {"Ganador":0, "Perdedor":0, "Empate":0} 
+    O = {"Ganador":0, "Perdedor":0, "Empate":0}
+    score = {"X":X, "O":O}
     numeros = [str(i) for i in range(1, 10)]
-    diccionario = {numero: numero for numero in numeros}
-    ganador = tablero.juego(diccionario)
-    if ganador is None:
-        print("Empate")
-    else:
-        print(f"El ganador es {ganador}")
+    
+    corriendo = True
+    while corriendo:
+        diccionario = {numero: numero for numero in numeros}
+        ganador = tablero.juego(diccionario)
+        if ganador is not None:
+            print(f"El ganador es {ganador}")
+            if ganador == "X":
+                X["Ganador"] += 1
+                O["Perdedor"] += 1
+            elif ganador == "O":
+                O["Ganador"] += 1
+                X["Perdedor"] += 1
+        else:
+            print("Empate")
+            X["Empate"] += 1
+            O["Empate"] += 1
+    
 
 if __name__ == '__main__':
     main()
