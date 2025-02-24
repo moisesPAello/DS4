bcf2fd163728fdc41e14c381f7ba69b234542288
moisesPAello <a223208792@unison.mx>
2025-02-24 14:48:35 -0700

    Fix: game.to_json()

--- games/Game.py
+++ games/Game.py
@@ -13,25 +13,32 @@ class Game:
         }
     def __init__(self, A:Team, B:Team):
         """Constructor de la clase Game"""
-        self.equipo1 = A
-        self.equipo2 = B
+        self.A = A
+        self.B = B
         self.score = dict()
         self.score[A.name] = 0
         self.score[B.name] = 0
 
     def play(self):
         """Juego simulado entre dos euipos"""
-        league = self.equipo1.sport.league
+        league = self.A.sport.league
         points = self.sports_dict[league]
         puntos1 = choice(points)
         puntos2 = choice(points)
-        self.score[self.equipo1.name] = puntos1
-        self.score[self.equipo2.name] = puntos2
+        self.score[self.A.name] = puntos1
+        self.score[self.B.name] = puntos2
 
     def __str__(self):
         """Metodo para representar la clase como string"""
-        return f"Game: {self.equipo1.name}: {self.score[self.equipo1.name]} - {self.score[self.equipo2.name]} :{self.equipo2.name}"
+        return f"Game: {self.A.name}: {self.score[self.A.name]} - {self.score[self.B.name]} :{self.B.name}"
         
+    def __repr__(self):
+        """Metodo para representar la clase como string"""
+        return f"Game(A={repr(self.A)}, B={repr(self.B)}), score)"
+
+    def to_json(self)->dict:
+        """Metodo para representar la clase como string"""
+        return {"A":self.A.to_json(), "B":self.B.to_json(), "score":self.score}
 
 if __name__ == "__main__":
     dt = ["Jordan", "Johnson", "Pipen", "Bird", "Kobe"]
@@ -45,5 +52,11 @@ if __name__ == "__main__":
     print(game)
     game.play()
     print(game)
+    print("----------------")
+    print(repr(game))
+    print(game.to_json())
+    filename_json =  "games/game.json"
+    with open(filename_json, "w", encoding="utf8") as f:
+        f.write(str(game.to_json()))
         
     
